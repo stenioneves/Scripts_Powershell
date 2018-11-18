a82f21b7724014cac682e1be59def1ed4a22a9e0
Stenio das Neves Silva <stenionevessilva@gmail.com>
2018-11-17 23:49:54 -0200

    Array- Testando os DNS da Google

---
+++ Google DNS-Teste usando Array.ps1
@@ -0,0 +1,7 @@
+﻿# Script com Array
+Clear-Host
+#Para criar um array deve começar com um @(
+$GoogleDNS = @("8.8.8.8","8.8.4.4")
+$TotalDNS= $GoogleDNS.Count
+Write-Host Pigando todos os  $TotalDNS DNS do Google
+Test-Connection $GoogleDNS -Count 1
