b5cb3dc00c17ddbc86dcad2a3396f39f1c3c0fa4
Stenio das Neves Silva <stenionevessilva@gmail.com>
2018-11-18 18:22:35 -0200

    Alterando rede e Colocando no dominio

---
+++ RedeEDomio.ps1
@@ -0,0 +1,13 @@
+﻿
+#obetendo a informação de configuração
+Get-NetIPConfiguration
+
+#Configurar endereço de IP
+#PrefixLength CIDR,wildcard e subnet mask
+New-NetIPAddress  -InterfaceAlias  -DefaultGateway -AddressFamily IPv6 -PrefixLength 24
+Set-DnsClientServerAddress -InterfaceAlias  -ServerAddresses
+#DHCP
+Set-NetIPInterface -InterfaceAlias -Dhcp Enabled
+Set-DnsClientServerAddress -InterfaceAlias  -ResetServerAddresses
+#Adcionar máquina ao dominio
+Add-Computer -ComputerName Elysium -DomainName "Seu dominio"
