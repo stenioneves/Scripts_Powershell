4c80754a5672b909d905c2f6ed9741207875a874
Stenio das Neves Silva <stenionevessilva@gmail.com>
2018-11-18 14:57:02 -0200

    Criando WScript

---
+++ WscriptShell.ps1
@@ -0,0 +1,16 @@
+﻿#Exemplo WScript.Shell
+
+#Iniciou a variavel
+$wshell = New-Object  -ComObject WScript.Shell
+#vendo as funções disponiveis
+$wshell | Get-Member
+# Exibir um Popup
+$wshell.Popup("Curso muito legal")
+#Iniciando uma aplicação
+$wshell.Run("Notepad")
+#Selecionado a app ativa
+$wshell.AppActivate("Notepad")
+#Aguardando 2 seg
+Start-Sleep 2
+#Enviado a String para o bloco de notas
+$wshell.SendKeys("Olá Stenio")
