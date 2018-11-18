1ad32933cb2d7094821a23331695ff767415c5f6
Stenio das Neves Silva <stenionevessilva@gmail.com>
2018-11-17 22:43:58 -0200

    Teste com Expressão Regular

---
+++ ExpressaoRegular.ps1
@@ -0,0 +1,11 @@
+﻿#Expressão Regular REGEX
+Clear-Host
+$email= Read-Host Qual é o seu e-mail ?
+$ExpreR="^[a-z]+\.[a=z]+@yourdomain.com$"
+
+if($email -notmatch $ExpreR){
+Write-Host "Endereço de e-mail errado $email"
+Exit
+
+}
+Write-Host E-mail valido !
