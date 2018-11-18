ecb4763d1c2096a33a0096e0b5958eb4f5c57ede
Stenio das Neves Silva <stenionevessilva@gmail.com>
2018-11-18 11:20:59 -0200

    Colocado o ELSE para teste de conexão à internet

--- TesteDeInternet.ps1
+++ TesteDeInternet.ps1
@@ -1,7 +1,6 @@
 ﻿# Teste de internet
 Clear-Host
 $conn= (Test-Connection www.google.com.br -Count 1 -Quiet)
-if($conn -eq "true")
-{
-Write-Host "Internet Funcionado" -ForegroundColor Green
-}
+if($conn -eq "true"){Write-Host "Internet Funcionado" -ForegroundColor Green}
+else{ Write-Host "Sem Internet X__X" -ForegroundColor Red}
+
