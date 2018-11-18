107617539f76c06457dc3ad262c50752fe70e3ca
Stenio das Neves Silva <stenionevessilva@gmail.com>
2018-11-18 18:24:44 -0200

    Gerenciando Active Directory (Deve ter o módulo do AD instalado na sua máquina para funcionar)

---
+++ GerenciamentoAD.ps1
@@ -0,0 +1,11 @@
+﻿#  Módulo  AD
+#Precisar do módulo AD instalado
+Get-Command -Module ActiveDirectory
+Get-Command new-ad*
+Get-Command Get-ad*
+Get-ADUser   -Properties 
+Get-ADUser -Filter * | where{$_.enable -eq $false}
+Get-ADUser -Filter * -Properties *| Export-Csv c:\listarUser.csv
+Get-ADUser -Filter * -Properties  * | select -Property GivenName, DisplayName
+Remove-ADUser 
+Import-Csv c:\listarUser.csv
