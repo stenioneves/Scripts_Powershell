be8ccb9801bf5aea9bf68eeb786ce9e91b42f3ef
Stenio das Neves Silva <stenionevessilva@gmail.com>
2018-11-18 21:43:06 -0200

    Script para colocar a máquina no dominio

---
+++ IngressarMaquinaNoDominio.ps1
@@ -0,0 +1,10 @@
+﻿################################################################################################
+#           Esse Script executa o processo de colocar a máquina no domínio                     #
+#                   Autor: Stenio  Neves  Data: 18/11/2018 Hora:20:14                          #
+################################################################################################
+
+$dom=Read-Host Informe o domínio a ingressar
+$user =Read-Host Informe o seu usuário administrativo
+$cred= Get-Credential $dom\$user
+$info = Get-WmiObject -Class Win32_ComputerSystem
+Add-Computer -ComputerName $info.Name -DomainName $dom -Credential $cred
