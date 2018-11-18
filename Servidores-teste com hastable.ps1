02f433b392b78e9cc9e78116a145a05d25231274
Stenio das Neves Silva <stenionevessilva@gmail.com>
2018-11-18 00:11:57 -0200

    Hash table -Teste com servidores Adicao e remoção

--- Servidores-teste com hastable.ps1
+++ Servidores-teste com hastable.ps1
@@ -1,2 +1,16 @@
 ﻿#Teste com Hash table
 Clear-Host
+#Atenção em Hash table é @{
+$Servidores = [ordered]@{Server1="127.0.0.1";Server2="192.168.10.106";Server3="192.168.10.103"}
+$Servidores
+#Adicionar
+$Servidores["Server4"]="192.168.10.1"
+#Remover
+$Servidores.Remove("Server4")
+
+$Servidores.Count
+
+Test-Connection $Servidores.Server3
+
+#Exibir valores da tabela hash
+$Servidores.Values
