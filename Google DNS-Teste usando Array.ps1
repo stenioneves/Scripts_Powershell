29c6b41c6d2c2ed2e1de2108cea543453aa6b497
Stenio das Neves Silva <stenionevessilva@gmail.com>
2018-11-17 23:52:48 -0200

    Adição do Comando sleep

--- Google DNS-Teste usando Array.ps1
+++ Google DNS-Teste usando Array.ps1
@@ -5,3 +5,5 @@ $GoogleDNS = @("8.8.8.8","8.8.4.4")
 $TotalDNS= $GoogleDNS.Count
 Write-Host Pigando todos os  $TotalDNS DNS do Google
 Test-Connection $GoogleDNS -Count 1
+sleep 3
+Write-Host Fim do script
