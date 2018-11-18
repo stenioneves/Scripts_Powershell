b5951b61f54ed5d0faa796fb9dc2543f4306d04f
Stenio das Neves Silva <stenionevessilva@gmail.com>
2018-11-18 12:36:49 -0200

    Comando Foreach

---
+++ FOREACH.ps1
@@ -0,0 +1,12 @@
+﻿#FOREACH
+Clear-Host
+foreach ($numeros in 1,2,3,4,5,6,7,8,9,10,100,1000,10000) {Write-Host $numeros}
+foreach($arquivos in Get-ChildItem) {
+
+if($arquivos.IsReadOnly){
+ Write-Host $arquivos.FullName}
+
+}
+#mais exemplo
+Get-Process Notepad
+Get-Process Notepad | foreach kill
