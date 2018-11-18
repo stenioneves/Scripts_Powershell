7e61ce0f3c63ce4a2a32d1d28c71c44aa17aa1d4
Stenio das Neves Silva <stenionevessilva@gmail.com>
2018-11-18 19:54:00 -0200

    Script para renomear o nome do computador

---
+++ RenomerPersonalizado.ps1
@@ -0,0 +1,10 @@
+﻿#renomer máquina Personalizado
+# Autor:Stenio
+
+$info = Get-WmiObject -Class Win32_ComputerSystem
+$wshell = New-Object  -ComObject WScript.Shell
+$name= Read-Host Qua nome que deseja colocar na sua máquina ?
+$info.Rename("$name")
+$wshell.Popup("Sua máquina irá reiniciar em 5 segundos !")
+Start-Sleep 5
+Restart-Computer
