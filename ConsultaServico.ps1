546581d400b07faf958a22675e27de8593316217
Stenio das Neves Silva <stenionevessilva@gmail.com>
2018-11-18 11:08:39 -0200

    IF e ELSE para teste de serviço em exeção

---
+++ ConsultaServico.ps1
@@ -0,0 +1,11 @@
+﻿#Consultar o serviço Windows
+Clear-Host
+$Serv= Get-Service -Name Spooler
+if($Serv.Status -eq "stopped")
+{ 
+Write-Host  "Serviço Parado"
+}
+else
+{
+Write-Host "Serviço em execução"
+}
