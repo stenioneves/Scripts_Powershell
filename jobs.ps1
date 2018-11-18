8918086c23b924efdeafbf4b38220f27d9534c75
Stenio das Neves Silva <stenionevessilva@gmail.com>
2018-11-18 13:37:24 -0200

    Jobs

---
+++ jobs.ps1
@@ -0,0 +1,14 @@
+﻿#Jobs
+WorkFlow WorkflowDemorado
+{
+while (1)
+     {
+     (Get-Date).ToString() + "Script demoradinho"
+     Start-Sleep -Seconds 2
+     }
+}
+$wfjob = WorkflowDemorado -AsJob
+Receive-Job $wfjob
+Suspend-Job $wfjob -Force
+Stop-Job $wfjob
+Resume-Job $wfjob
