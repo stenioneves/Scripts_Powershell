208597178f194b0b4374709aa9531755c6d16f83
Stenio das Neves Silva <stenionevessilva@gmail.com>
2018-11-18 13:24:56 -0200

    Diferença entre Função e Workflow

---
+++ WorflowVSFunction.ps1
@@ -0,0 +1,18 @@
+﻿#Diferença entre Função e Workflow
+#Função executa os  processo em seguência.
+function iniciar-editores
+{
+Start-Process Notepad
+sleep 5
+Start-Process Wordpad
+}
+
+#Workflow pode ser executado em paralelo
+workflow start-editores
+{
+parallel {
+Start-Process Notepad
+sleep 5
+Start-Process Wordpad
+}
+}
