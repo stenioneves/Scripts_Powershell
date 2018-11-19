88aaf0c20ce32548fb472a490614b60a02f56155
Stenio das Neves Silva <stenionevessilva@gmail.com>
2018-11-18 22:11:48 -0200

    Configuração do AppvClient na estação<em teste>

---
+++ AppvClientConfigServer.ps1
@@ -0,0 +1,9 @@
+﻿################################################################################################
+#           Esse Script executa o processo de configurar o appvclient                          #
+#                   Autor: Stenio  Neves  Data: 18/11/2018 Hora:21:47                          #
+################################################################################################
+Import-Module appvclient
+$server=""
+$url =""
+
+Add-AppvPublishingServer -Name $server -Url $url
